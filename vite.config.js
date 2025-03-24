e0e8971d6851031c93e72417dd1891367bf94aac
Akshidhan <wakshiwije@gmail.com>
2025-03-25 01:23:05 +0530

    Deployed

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  publicDir: 'public' // Ensure this is set correctly
+  base: '/product-list-with-cart',
+  plugins: [react()]
 })
